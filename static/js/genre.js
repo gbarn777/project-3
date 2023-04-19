35d47b094b068742af4cec92555359802ecc80c5
Miguel-Ari <arim0391@gmail.com>
2023-04-18 21:37:10 -0400

    genre updates with additional plot

--- static/js/genre.js
+++ static/js/genre.js
@@ -5,6 +5,7 @@ const url = "http://localhost:5000/api/movies"
 d3.json(url).then(function(data) {
   console.log(data);
   processData(data);
+  processData2(data);
 }
 );
 
@@ -73,6 +74,7 @@ function createPlot() {
 
     // Set the layout for the plot
     const layout = {
+      title: 'Number of Movies By Genre',
       xaxis: {
         title: 'Release Year'
       },
@@ -82,6 +84,114 @@ function createPlot() {
     };
 
     // Create the plot
-    Plotly.newPlot('plot', traces, layout);
+    Plotly.newPlot('plot1', traces, layout);
+}
+
+
+
+
+
+
+// const url = "http://localhost:5000/api/movies";
+
+// // Call the API to get movies data
+// d3.json(url).then(function(data) {
+//   console.log(data);
+//   processData2(data);
+// });
+
+function processData2(data) {
+  // Group movies by year
+  const moviesByYear = groupMoviesByYear2(data);
+
+  // Group movies by genre
+  const moviesByGenre = groupMoviesByGenre2(data);
+
+  // Get the dropdown element
+  const genreDropdown = document.getElementById("genre-select");
+
+  // Set the default selected value
+  const defaultGenre = "Animation";
+  genreDropdown.value = defaultGenre;
+
+  // Add an event listener to the dropdown menu
+  genreDropdown.addEventListener("change", function(event) {
+    const selectedGenre = event.target.value;
+    console.log("Selected genre:", selectedGenre);
+
+    // Filter movies by selected genre
+    const filteredMovies = moviesByGenre[selectedGenre];
+
+    // Group filtered movies by year
+    const filteredMoviesByYear = groupMoviesByYear2(filteredMovies);
+
+    // Create the plot
+    createPlot2(filteredMoviesByYear, selectedGenre);
+  });
+
+  // Create the plot
+  createPlot2(moviesByYear, defaultGenre);
+}
+
+function groupMoviesByYear2(movies) {
+  // Create an object to hold the movies by year
+  const moviesByYear = {};
+
+  // Loop through the movies and group them by year
+  movies.forEach(movie => {
+    const year = new Date(movie.release_date).getFullYear();
+    if (!moviesByYear[year]) {
+      moviesByYear[year] = [];
+    }
+    moviesByYear[year].push(movie);
+  });
+
+  // Return the movies grouped by year
+  console.log(moviesByYear);
+  return moviesByYear;
+}
+
+function groupMoviesByGenre2(movies) {
+  // Create an object to hold the movies by genre
+  const moviesByGenre = {};
+
+  // Loop through the movies and group them by genre
+  movies.forEach(movie => {
+    const genre = movie.genre;
+    if (!moviesByGenre[genre]) {
+      moviesByGenre[genre] = [];
+    }
+    moviesByGenre[genre].push(movie);
+  });
+
+  // Return the movies grouped by genre
+  console.log(moviesByGenre);
+  return moviesByGenre;
+}
+
+function createPlot2(data, genre) {
+  const xData = Object.keys(data);
+
+  // Create the traces for the genre
+  const traces = [{
+    x: xData,
+    y: xData.map(year => data[year].length),
+    name: genre,
+    type: 'bar'
+  }];
+
+  // Set the layout for the plot
+  const layout = {
+    title: `Number of ${genre} Movies By Year`,
+    xaxis: {
+      title: 'Release Year'
+    },
+    yaxis: {
+      title: 'Number of Movies'
+    }
+  };
+
+  // Create the plot
+  Plotly.newPlot('plot2', traces, layout);
 }
 
