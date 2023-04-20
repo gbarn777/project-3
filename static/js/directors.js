c2213994bc22b2e43f2d0f9c3462e49122816873
gracebarnett <gracebarnett288@gmail.com>
2023-04-20 16:14:44 -0400

    adding directors code

---
+++ static/js/directors.js
@@ -0,0 +1,79 @@
+console.log("data")
+
+// Fetch data from the API endpoint
+fetch('/api/directors')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    // Extract data for the chart
+    const labels = data.map(movie => movie.title);
+    const revenues = data.map(movie => movie.revenue);
+
+    // Create a data object for the chart
+    const chartData = {
+      labels: labels,
+      datasets: [{
+        label: 'Movies',
+        data: data.map(movie => ({
+          x: movie.title,
+          y: movie.revenue,
+          r: 10 // Set bubble radius
+        })),
+        backgroundColor: 'rgba(75, 192, 192, 0.5)', // Set bubble background color
+        borderColor: 'rgba(75, 192, 192, 1)', // Set bubble border color
+        borderWidth: 1, // Set bubble border width
+        hoverRadius: 10, // Set hover radius for bubbles
+        hoverBorderWidth: 2 // Set hover border width for bubbles
+      }]
+    };
+
+    // Create a chart using Chart.js
+    new Chart(document.getElementById('bubbleChart'), {
+      type: 'bar',
+      data: chartData,
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: 'Movie Title' // Set x-axis label
+            },
+            ticks: {
+              callback: function(value, index, values) {
+                // Remove director name from movie title
+                return value.split(' (')[0];
+              }
+            }
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Revenue' // Set y-axis label
+            }
+          }
+        }
+      }
+    });
+  })
+  .catch(error => console.error('Error fetching data:', error));
+
+console.log("movies-bubblechart.js")
+
+var yearTag = d3.select('#selYear');
+
+// Add dropdown menu
+// Fetch data from the API and create the chart
+fetch('/api/directors')
+  .then(response => response.json())
+  .then(data => {
+    createChart(data);
+    // Add an event listener to update the chart when the language selection changes
+    const directorSelector = document.getElementById('directorSelector');
+    Selector.addEventListener('change', event => {
+      updateChart(event.target.value);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching directors:', error);
+  });
